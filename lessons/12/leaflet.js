b370774cd2842dac60dcd2888d63f0c6a92958c6
Vladimir Klepov <v.klepov@gmail.com>
2017-05-21 00:57:14 +1100

    optimize 12

--- lessons/12/leaflet.js
+++ lessons/12/leaflet.js
@@ -1,4 +1,5 @@
 const map = L.map('mapid');
+const canvasRenderer = L.canvas();
 map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
 
 // Получить все значения из объекта
@@ -19,76 +20,160 @@ const clearFeatures = (featureArr, map) => {
   featureArr.forEach(f => f.removeFrom(map));
   featureArr.length = 0;
 };
+const patchFeatures = (featureArr, dataArr, patch, create, map) => {
+  featureArr._group = featureArr._group || (new L.LayerGroup({ renderer: canvasRenderer })).addTo(map);
+  const group = featureArr._group;
+
+  const extraData = dataArr.length - featureArr.length;
+  for (let i = 0; i < extraData; i++) {
+    const layer = create();
+    group.addLayer(layer);
+    featureArr.push(layer);
+  }
+  for (let i = 0; i < -extraData; i++) {
+    group.removeLayer(featureArr[featureArr.length - i - 1]);
+  }
+
+  dataArr.forEach((item, i) => {
+    const layer = featureArr[i];
+    patch(layer, item);
+    if (!group.hasLayer(layer)) {
+      group.addLayer(layer);
+    }
+  });
+};
+const includesPair = (arr, e1, e2) => {
+  const L = arr.length;
+  for (let i = 0; i < L; i++) {
+    if (arr[i][0] === e1 && arr[i][1] === e2 || arr[i][1] === e2 && arr[i][0] === e1) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // посчитать кластеры по остановкам
-const computeClusters = (stops, D) => {
+const computeClusters = (network, D) => {
+  const stops = network.stops;
   const clusters = [];
   stops.forEach(stop1 => {
-    const nearClust = clusters.find(clust =>
-      clust.stops.some(stop2 => d(stop1, stop2) < D)
-    );
+    const nearClust = clusters.find(clust => {
+      if (d(clust.centroid, stop1) > 2 * D) return;
+      for (var i = 0; i < clust.stops.length; i++) {
+        const stop2 = clust.stops[i];
+        if (network.stopDistances[stop1.id][stop2.id] < D) {
+          return true;
+        }
+      }
+      return false;
+    });
     if (nearClust) {
       nearClust.stops.push(stop1);
     } else {
-      clusters.push({ stops: [stop1] });
+      clusters.push({ stops: [stop1], centroid: centroid([stop1]) });
     }
   });
-  clusters.forEach(c => {
-    c.routes = c.stops.reduce((a, stop) => a.concat(stop.routes), []);
-  });
+  // accelerators
+  // clusters.forEach(c => {
+  //   c.routes = c.stops.reduce((a, stop) => a.concat(stop.routes), []);
+  // });
   return clusters;
-}
+};
+
+const computeDistances = (stops) => {
+  const cache = {};
+  stops.forEach((stop) => {
+    cache[stop.id] = {};
+    stops.forEach((stop2) => {
+      cache[stop.id][stop2.id] = d(stop, stop2);
+    });
+  });
+  return cache;
+};
+
+const computeStopEdges = (routes) => {
+  const stopEdges = [];
+  routes.forEach((r) => {
+    values(r.trips).forEach(t => {
+      t.stops.slice(1).forEach((s, i) => {
+        const prev = t.stops[i];
+        if (!includesPair(stopEdges, prev, s)) {
+          stopEdges.push([prev, s]);
+        }
+      });
+    });
+  });
+  return stopEdges;
+};
 
 // полностью перерисовать карту
-const renderNetwork = (stops, routes, map, options = {}) => {
-  const clusters = computeClusters(stops, options.D);
+const renderNetwork = (network, map, options = {}) => {
+  const clusters = computeClusters(network, options.D);
 
-  renderClusters(clusters, map);
+  renderClusters(clusters, map, options.D);
 
   // renderRoutes(routes.map(r => values(r.trips)[0]), map);
-  renderClusterLinks(clusters, map);
+  renderClusterLinks(clusters, network, map);
 
   // сбросить выбранный кластер - его уже нет на карте
   renderActiveCluster(null, map);
 }
 
 // нарисовать связи меду кластерами
-const renderClusterLinks = (clusters, map) => {
+const clusterLinkFeatures = [];
+const renderClusterLinks = (clusters, network, map) => {
   const clusterLinks = [];
-  clusters.forEach(c => {
-    clusters
-      .filter(c2 => c.routes.some(r => c2.routes.includes(r)))
-      .forEach(c2 => clusterLinks.push([c, c2]));
+  const stopIdToCluster = {};
+  clusters.forEach((c, id) => {
+    c.id = id;
+    c.stops.forEach(stop => {
+      stopIdToCluster[stop.id] = c;
+    });
   });
-  clusterLinks.forEach(link => {
+  const pairHash = {};
+  network.stopEdges.forEach(([fromId, toId]) => {
+    const cFrom = stopIdToCluster[fromId];
+    const cTo = stopIdToCluster[toId];
+
+    const pairId = cFrom.id < cTo.id ? `${cFrom.id}:${cTo.id}` : `${cTo.id}:${cFrom.id}`;
+    if (!pairHash[pairId]) {
+      clusterLinks.push([cFrom, cTo]);
+      pairHash[pairId] = true;
+    }
+  });
+  const makeClusterLink = () => L.polyline([], { color: 'white', weight: 4 });
+  const patchClusterLink = (f, link) => {
     const c0 = centroid(link[0].stops);
     const c1 = centroid(link[1].stops);
-    const line = [[c0.lat, c0.long], [c1.lat, c1.long]];
-    stopsOnMap.push(L.polyline(line, { color: 'white', weight: 1 }).addTo(map));
-  });
+    f.setLatLngs([[c0.lat, c0.long], [c1.lat, c1.long]]);
+  };
+  patchFeatures(clusterLinkFeatures, clusterLinks, patchClusterLink, makeClusterLink, map);
 };
 
 // Нарисовать все кластеры
 const stopsOnMap = [];
-const renderClusters = (clusters, map) => {
-  clearFeatures(stopsOnMap, map);
-  clusters.forEach(cluster => {
+const makeClusterMarker = () => L.circle([0, 0], { stroke: false, color: 'white' });
+const renderClusters = (clusters, map, D) => {
+  const patchClusterMarker = (f, cluster) => {
     const stop = centroid(cluster.stops); // Рисуем в центре кластера
-    const marker = L.circleMarker([stop.lat, stop.long], {
-      stroke: false,
-      fillOpacity: 0.8,
-      radius: cluster.stops.length, // точку, размер которой показывает размер кластера
-      color: 'white',
-    })
-    .addTo(map)
-    .on('click', () => renderActiveCluster(cluster, map)); // по клику на кластер показывем все остановки в нем
-    stopsOnMap.push(marker); // сохраняем, чтобы удалить при перерисовке
-  });
+    f.setLatLng(
+      new L.LatLng(stop.lat, stop.long)
+    ).setStyle({
+      fillOpacity: cluster.stops.length < 3 ? 0.6 : 0.8,
+    }).setRadius(
+      100 + 30 * cluster.stops.length, // точку, размер которой показывает размер кластера
+    ).on(
+      'click',
+      () => renderActiveCluster(cluster, map, D)
+    ); // по клику на кластер показывем все остановки в нем
+  };
+  patchFeatures(stopsOnMap, clusters, patchClusterMarker, makeClusterMarker, map);
+  // console.log(stopsOnMap);
 };
 
 // Отрисовать остановки в выбранном кластере
 const activeCluster = [];
-const renderActiveCluster = (cluster, map) => {
+const renderActiveCluster = (cluster, map, D) => {
   clearFeatures(activeCluster, map);
   if (!cluster) return;
   // для каждой остановки в кластере
@@ -101,7 +186,9 @@ const renderActiveCluster = (cluster, map) => {
       color: 'black',
     }).addTo(map));
     // и соединяем со всеми остальными остановками кластера
-    cluster.stops.forEach(stop2 => {
+    cluster.stops.filter(stop2 => {
+      return d(stop, stop2) < D;
+    }).forEach(stop2 => {
       const shape = [[stop.lat, stop.long], [stop2.lat, stop2.long]];
       activeCluster.push(L.polyline(shape, {
         color: 'red',
@@ -152,12 +239,18 @@ const startApp = (data, map) => {
   map.setView(calculateViewportCenter(stops), 11);
 
   // привязываем отрисовку к интерфейсу
+  const network = {
+    stops,
+    routes,
+    stopEdges: computeStopEdges(routes),
+    stopDistances: computeDistances(stops),
+  };
   const clusterControl = document.getElementById('cluster-radius');
   clusterControl.addEventListener('input', () => {
     // получаем значение ползунка, управляющего размером кластера
     const clusterSize = Number(clusterControl.value);
     // перерисовываем маршруты
-    renderNetwork(stops, routes, map, { D: clusterSize });
+    renderNetwork(network, map, { D: clusterSize });
   });
 };
 
